b0901217c8a16e2ecacab9caabf284a98f444205
EzraNouval <ezranouval@gmail.com>
2021-02-26 10:00:55 +0700

    tugas kedua

--- soal-1.py
+++ soal-1.py
@@ -1,7 +1,6 @@
-
-
 print("Selamat Datang!")
 print("===============")
+
 while True:
     print("---menu---")
     print("1. Daftar Kontak")
@@ -11,24 +10,26 @@ while True:
     menu = input("Pilih menu: ")
 
     if menu == "1":
-        kontak = {}
-        for x in kontak:
-            print("======================")
-            print(f"Nama: {kontak['nama']}")
-            print(f"Telepon: {kontak['telepon']}")
-            print(kontak)
+        dict = {}
+        kontak = ['nama'== 'nama', 'telepon' == 'telepon']
+        kontak.append(print(kontak))
+
+        
     elif menu == "2":
         nama = input("Masukkan nama: ")
         telepon = input("Masukkan telepon: ")
         kontak = {
-        "nama" == nama,
-        "telepon" == telepon
+        "nama" : nama,
+        "telepon" : telepon
         }        
-        print(kontak)
+        for x in kontak:
+            print(kontak[x])
         print("Kontak berhasil ditambahkan")
+        
     elif menu == "3":
-        print("program selesai, sampai jumpa!")
+        print("program selesai, sampai jumpa!") 
         break
+
     else:
         print("menu tidak tersedia")
         break
